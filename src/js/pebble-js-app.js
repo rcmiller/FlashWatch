aceb286a67398b86a5e7bd42084b0579a99a85bc
rcmiller <rcm@mit.edu>
2014-04-27 15:51:28 -0700

    Phone side now chooses next flashcard

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,11 +1,34 @@
+var flashcards = [
+    { front: "riesling", back: "sweet white, off-dry apricots peaches" },
+    { front: "sancerre", back: "dry white, light herbal grassy" },
+    { front: "pinot grigio", back: "dry white, light citrus lemon" },
+    { front: "pinot blanc", back: "dry white, light grapefruit floral" },
+    { front: "cotes du rhone", back: "fruity red, strawberry cherry, round" },
+    { front: "cabernet sauvignon", back: "fruity red, black cherry raspberry, high tannin" },
+    { front: "shiraz", back: "fruity red, blueberry blackberry, spicy" },
+    { front: "chianti", back: "savory red, clay cured meats, high tannin" },
+    { front: "pinot noir", back: "fruity red, strawberry cherry, round" },
+    { front: "merlot", back: "fruity red, black cherry raspberry, round" }
+];
+
+
+function pickAnyCard(cards) {
+    var i = Math.floor(Math.random() * cards.length);
+    return cards[i];    
+}
+
+function sendCard(card) {
+    Pebble.sendAppMessage({
+        "front": card.front,
+         "back": card.back,
+         "result": 0,
+    });
+}
+
 Pebble.addEventListener("ready",
                         function(e) {
-                            console.log("ready: " + e.ready); 
-                            Pebble.sendAppMessage({
-                                "front": "riesling",
-                                "back": "off-dry white",
-                                "result": 0,
-                            });
+                            console.log("ready: " + e.ready);
+                            sendCard(pickAnyCard(flashcards));
                         });
 
 Pebble.addEventListener("appmessage",
@@ -15,11 +38,7 @@ Pebble.addEventListener("appmessage",
                                         + ", back=" + e.payload.back
                                         + ", result=" + e.payload.result
                                         );
-                            Pebble.sendAppMessage({
-                                "front": "sancerre",
-                                "back": "dry white",
-                                "result": 0,
-                            });
+                            sendCard(pickAnyCard(flashcards));
                         });
 
  
--- src/main.c
+++ src/main.c
@@ -1,17 +1,27 @@
 #include <pebble.h>
 
+// forward declarations
+void connection_send_result();
+    
+    
 //// UI elements 
-Window *window;
-TextLayer *front_layer;
-TextLayer *back_layer;
+Window *window = NULL;
+TextLayer *front_layer = NULL;
+TextLayer *back_layer = NULL;
 
 
 //// Flashcard data representation
 
 typedef struct {
-  char * front;
-  char * back;
+  const char * front;
+  const char * back;
 } FlashCard;
+
+FlashCard theCard = { "starting", "initial" };
+
+
+
+//// Some hard-coded cards
 
 FlashCard cards[] = {
   { "riesling", "sweet white, off-dry apricots peaches" },
@@ -27,6 +37,104 @@ FlashCard cards[] = {
 };
 int num_cards = sizeof(cards) / sizeof(cards[0]);
 
+
+FlashCard* pick_any_card() {
+    // pick a card to show
+    return &cards[rand() % num_cards];
+}
+
+
+//// Displaying text on screen
+
+char* fonts[] = {
+  FONT_KEY_BITHAM_42_BOLD,
+  FONT_KEY_BITHAM_30_BLACK,
+  FONT_KEY_GOTHIC_28_BOLD,
+  FONT_KEY_GOTHIC_24_BOLD,
+  FONT_KEY_GOTHIC_18_BOLD
+};
+int num_fonts = sizeof(fonts) / sizeof(fonts[0]);
+
+// Make a text layer half the height of the screen,
+// and set its font as big as possible that still 
+// keeps the text visible 
+TextLayer* make_text(const char* text, bool is_front) {
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
+  	//APP_LOG(APP_LOG_LEVEL_ERROR, "bounds of window: x=%d, y=%d, w=%d, h=%d", 
+    //        bounds.origin.x, bounds.origin.y, bounds.size.w, bounds.size.h);
+    int16_t screen_height = bounds.size.h;
+    int16_t half_screen_height = screen_height/2;
+  
+	  TextLayer* layer = text_layer_create(GRect(0, is_front ? 0 : half_screen_height, 144, screen_height));
+  	text_layer_set_text(layer, text);
+  	text_layer_set_text_alignment(layer, GTextAlignmentLeft);
+
+    // step down font size until the text fits in half a screen height
+    for (int i = 0; i < num_fonts; ++i) {
+      text_layer_set_font(layer, fonts_get_system_font(fonts[i]));
+      GSize size = text_layer_get_content_size(layer);
+      //APP_LOG(APP_LOG_LEVEL_ERROR, "%s occupies h=%d w=%d", fonts[i], size.h, size.w);
+      if (size.h <= half_screen_height) break;        
+    } // if we finished the loop without finding a small enough font, just live with it
+  
+    text_layer_set_background_color(layer, is_front ? GColorWhite : GColorBlack);  
+    text_layer_set_text_color(layer, is_front ? GColorBlack : GColorWhite);  
+    return layer;
+}
+
+
+//// Displaying a flashcard
+
+void flashcard_create(FlashCard *card) {
+
+  	// create text layers for the front and back of the flashcard
+    front_layer = make_text(card->front, true);  
+    back_layer = make_text(card->back, false);
+  
+    // put the front and back into the window
+  	layer_add_child(window_get_root_layer(window), text_layer_get_layer(front_layer));
+  	layer_add_child(window_get_root_layer(window), text_layer_get_layer(back_layer));
+  
+    // randomly hide either the front or the back at first
+    bool hide_front_at_first = (rand() % 2 > 0);
+    layer_set_hidden(text_layer_get_layer(hide_front_at_first ? front_layer : back_layer), true);
+  
+    // wake up the user
+    //vibes_short_pulse();
+    light_enable_interaction();
+}
+
+void flashcard_destroy() {    
+    if (front_layer) text_layer_destroy(front_layer);
+	if (back_layer) text_layer_destroy(back_layer);
+    front_layer = back_layer = NULL;
+}
+
+
+//// Handling button input
+
+// invoked when the user presses the Select button (middle button)
+// gives the answer to the flashcard
+void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+    // if flashcard is already showing, make a new one
+    if (!layer_get_hidden(text_layer_get_layer(front_layer)) 
+        && !layer_get_hidden(text_layer_get_layer(back_layer))) {
+
+        // send result and request a new flashcard
+        flashcard_destroy();
+        connection_send_result();
+    } else {
+      // make both layers visible
+      layer_set_hidden(text_layer_get_layer(front_layer), false);
+      layer_set_hidden(text_layer_get_layer(back_layer), false);
+    }
+}
+
+void click_config_provider(void *context) {
+    window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+    
+}
 
 
 //// Communication with phone
@@ -45,7 +153,7 @@ enum {
 };
 
 struct AppSync appsync;
-uint8_t* sync_buffer;
+uint8_t* sync_buffer = NULL;
 int sync_buffer_size = 500; // bytes
 
 char *translate_error(AppMessageResult result) {
@@ -68,8 +176,25 @@ char *translate_error(AppMessageResult result) {
   }
 }
 
+void show_flashcard_sent_by_phone() {
+    const Tuple* front_tuple = app_sync_get(&appsync, FLASH_KEY_FRONT);
+    const Tuple* back_tuple = app_sync_get(&appsync, FLASH_KEY_BACK);
+    if (!front_tuple || !back_tuple) {
+        APP_LOG(APP_LOG_LEVEL_ERROR, "front tuple or back tuple is null");
+        return;
+    }
+    theCard.front = front_tuple->value->cstring;
+    theCard.back = back_tuple->value->cstring;
+    
+    flashcard_destroy();
+    flashcard_create(&theCard);
+}
+
 void connection_tuple_changed(const uint32_t key, const Tuple *new_tuple, const Tuple *old_tuple, void *context) {
-    APP_LOG(APP_LOG_LEVEL_ERROR, "tuple changed: key=%u, value=%s", (unsigned) key, new_tuple->value->cstring);    
+    APP_LOG(APP_LOG_LEVEL_ERROR, "tuple changed: key=%u, value=%s", (unsigned) key, new_tuple->value->cstring);
+    if (key == FLASH_KEY_RESULT) {
+        show_flashcard_sent_by_phone();
+    }
 }
 
 void connection_error(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
@@ -114,125 +239,14 @@ void connection_destroy() {
     free(sync_buffer);
 }
 
-void connection_send_something() {
+void connection_send_result() {
     AppMessageResult result;
     Tuplet tuples[] = {
         TupletInteger(FLASH_KEY_RESULT, (uint8_t) DONE),
     };        
     result = app_sync_set(&appsync, tuples, sizeof(tuples)/sizeof(Tuplet));
-    APP_LOG(APP_LOG_LEVEL_ERROR, "app_sync_set returned %s", translate_error(result));  
-    
-#ifdef USE_APP_MESSAGE
-    DictionaryIterator *iter;
-    result = app_message_outbox_begin(&iter);
-    APP_LOG(APP_LOG_LEVEL_ERROR, "app_message_outbox_begin returned %s", translate_error(result));  
-    
-    if (iter == NULL) {
-        return;
-    }
-    
-    dict_write_tuplet(iter, &tuple);
-    dict_write_end(iter);
-    
-    result = app_message_outbox_send();    
-    APP_LOG(APP_LOG_LEVEL_ERROR, "app_message_outbox_send returned %s", translate_error(result)); 
-#endif
-    
+    APP_LOG(APP_LOG_LEVEL_ERROR, "app_sync_set returned %s", translate_error(result));      
 }
-
-//// Displaying text on screen
-
-char* fonts[] = {
-  FONT_KEY_BITHAM_42_BOLD,
-  FONT_KEY_BITHAM_30_BLACK,
-  FONT_KEY_GOTHIC_28_BOLD,
-  FONT_KEY_GOTHIC_24_BOLD,
-  FONT_KEY_GOTHIC_18_BOLD
-};
-int num_fonts = sizeof(fonts) / sizeof(fonts[0]);
-
-// Make a text layer half the height of the screen,
-// and set its font as big as possible that still 
-// keeps the text visible 
-TextLayer* make_text(char* text, bool is_front) {
-    Layer *window_layer = window_get_root_layer(window);
-    GRect bounds = layer_get_bounds(window_layer);
-  	//APP_LOG(APP_LOG_LEVEL_ERROR, "bounds of window: x=%d, y=%d, w=%d, h=%d", 
-    //        bounds.origin.x, bounds.origin.y, bounds.size.w, bounds.size.h);
-    int16_t screen_height = bounds.size.h;
-    int16_t half_screen_height = screen_height/2;
-  
-	  TextLayer* layer = text_layer_create(GRect(0, is_front ? 0 : half_screen_height, 144, screen_height));
-  	text_layer_set_text(layer, text);
-  	text_layer_set_text_alignment(layer, GTextAlignmentLeft);
-
-    // step down font size until the text fits in half a screen height
-    for (int i = 0; i < num_fonts; ++i) {
-      text_layer_set_font(layer, fonts_get_system_font(fonts[i]));
-      GSize size = text_layer_get_content_size(layer);
-      //APP_LOG(APP_LOG_LEVEL_ERROR, "%s occupies h=%d w=%d", fonts[i], size.h, size.w);
-      if (size.h <= half_screen_height) break;        
-    } // if we finished the loop without finding a small enough font, just live with it
-  
-    text_layer_set_background_color(layer, is_front ? GColorWhite : GColorBlack);  
-    text_layer_set_text_color(layer, is_front ? GColorBlack : GColorWhite);  
-    return layer;
-}
-
-
-//// Displaying a flashcard
-
-void flashcard_create() {
-
-    // pick a card to show
-    FlashCard *card = &cards[rand() % num_cards];
-  
-  	// create text layers for the front and back of the flashcard
-    front_layer = make_text(card->front, true);  
-    back_layer = make_text(card->back, false);
-  
-    // put the front and back into the window
-  	layer_add_child(window_get_root_layer(window), text_layer_get_layer(front_layer));
-  	layer_add_child(window_get_root_layer(window), text_layer_get_layer(back_layer));
-  
-    // randomly hide either the front or the back at first
-    bool hide_front_at_first = (rand() % 2 > 0);
-    layer_set_hidden(text_layer_get_layer(hide_front_at_first ? front_layer : back_layer), true);
-  
-    // wake up the user
-    //vibes_short_pulse();
-    light_enable_interaction();
-}
-
-void flashcard_destroy() {    
-	  text_layer_destroy(front_layer);
-	  text_layer_destroy(back_layer);
-}
-
-
-//// Handling button input
-
-// invoked when the user presses the Select button (middle button)
-// gives the answer to the flashcard
-void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-    connection_send_something();
-    // if flashcard is already showing, make a new one
-    if (!layer_get_hidden(text_layer_get_layer(front_layer)) 
-        && !layer_get_hidden(text_layer_get_layer(back_layer))) {
-        flashcard_destroy();
-        flashcard_create(window);
-    } else {
-      // make both layers visible
-      layer_set_hidden(text_layer_get_layer(front_layer), false);
-      layer_set_hidden(text_layer_get_layer(back_layer), false);
-    }
-}
-
-void click_config_provider(void *context) {
-    window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-    
-}
-
 
 //// App setup and teardown
 
@@ -244,7 +258,7 @@ void handle_init(void) {
   
     // make a window and the first flashcard
 	window = window_create();
-    flashcard_create();
+    flashcard_create(&theCard);
     
     // Listen for select button
     window_set_click_config_provider(window, click_config_provider);
